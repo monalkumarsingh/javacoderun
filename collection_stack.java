513daaac7727228fe60f9a86ba5ca357891de5d5
Monal Singh <145989131+monalkumarsingh@users.noreply.github.com>
2023-10-14 12:47:41 +0530

    Add files via upload

---
+++ collection_stack.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class collection_stack {
+    public static void main(String[] args) {
+     Stack<Integer> number=new Stack<>();
+     number.push(10);
+      number.push(20);
+       number.push(30);
+        number.push(40);
+         number.push(50);
+         System.out.println(number);
+         number.pop();
+         System.out.println(number);
+    }
+}
